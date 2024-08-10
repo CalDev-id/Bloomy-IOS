4ad29252d98914a0f9664adc1253ac747cf64ab8
Heical Chandra <haical31@gmail.com>
2024-08-10 21:24:38 +0700

    Initial commit

---
+++ Bloomy/BloomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomyApp.swift
+//  Bloomy
+//
+//  Created by Heical Chandra on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BloomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
